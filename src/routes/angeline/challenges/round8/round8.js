05ed093d292138a9e9c735aa8747a3db7c74b6dc
angelinewashburn <ballofcharisma@gmail.com>
2024-01-30 10:39:49 -0700

    Adding Capstone

--- src/routes/angeline/challenges/round8/round8.js
+++ src/routes/angeline/challenges/round8/round8.js
@@ -1,43 +1,66 @@
 export const challenge1 = (data) => {
 	// Get the average negative value with a standard For Loop
 	// variables
+	let paper = [];
 
 	for (let i = 0; i < data.length; i++) {
-		// code
+		if (data[i] < 0) {
+			paper.push(data[i]);
+		}
 	}
 
-	return '-';
+	let anotherPaper = 0;
+
+	for (let i = 0; i < paper.length; i++) {
+		anotherPaper += paper[i];
+	}
+
+	return anotherPaper / paper.length;
 };
 
 export const challenge2 = (data) => {
 	// Get the average negative value with a For-In Loop
 	// variables
-
+	let paper = [];
 	for (let i in data) {
-		// code
+		if (data[i] < 0) {
+			paper.push(data[i]);
+		}
+		// anotherPaper += paper[i];
 	}
-
-	return '-';
+	let anotherPaper = 0;
+	for (let i in paper) {
+		anotherPaper += paper[i];
+	}
+	return anotherPaper / paper.length;
 };
 
 export const challenge3 = (data) => {
 	// Get the average negative value with a For-Of Loop
 	// variables
-
+	let paper = [];
 	for (let value of data) {
-		// code
+		if (value < 0) {
+			paper.push(value);
+		}
 	}
 
-	return '-';
-};
-
+	let anotherPaper = 0;
+	for (let value of paper) {
+		anotherPaper += value;
+	} // 0 +-42 + -34 + -106 + - 153 =
+	return anotherPaper / paper.length;
+}; // / 4
 export const challenge4 = (data) => {
 	// Get the average negative value with a [].filter() method
 	// variables
+	let paper = data.filter((value) => value < 0);
 
-	data.filter((value) => {
-		// code
-	});
+	let anotherPaper = 0;
+	for (let value of paper) {
+		anotherPaper += value;
+	}
+	// code
 
-	return '-';
+	return anotherPaper / paper.length;
 };
