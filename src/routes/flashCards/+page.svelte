bdeccd2a5b8c3e350f3c2bdb80f63ac33c75e479
heatherhopkins <heatherhop77@gmail.com>
2024-02-08 13:03:14 -0700

    flash cards

--- src/routes/flashCards/+page.svelte
+++ src/routes/flashCards/+page.svelte
@@ -13,7 +13,61 @@
 </script>
 
 <!-- HTML area -->
+<head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Lexend:wght@700;800;900&display=swap"
+		rel="stylesheet"
+	/>
+	/>
+</head>
+<main>
+	{#each data.flash_cards as card}
+		<div class="flash-card">
+			{#each card.setup as value}
+				<div>{value}</div>
+			{/each}
+			{card.test}
+		</div>
+	{/each}
+</main>
 
 <style lang="postcss">
-	/* CSS Area */
+	/*CSS area*/
+	main {
+		display: flex;
+		flex-wrap: wrap;
+		flex-direction: row;
+		align-items: center;
+		color: #414c76;
+		background-color: #f2f3f8;
+	}
+	.flash-card {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		margin: 2em;
+		border: solid 2px #4e598c;
+		background-color: #f9c98b;
+		height: 350px;
+		width: 230px;
+		border-radius: 8px;
+		box-shadow: 2px 2px 4px #4e598c;
+		font-size: 18px;
+		padding-left: 35px;
+		padding-right: 35px;
+		text-align: center;
+		/*text-space: 10;*/
+		line-height: 2;
+		font-family: 'Lexend', sans-serif;
+	}
+	.flash-card:hover {
+		cursor: pointer;
+	}
+	.flash-card:active {
+		transform: translatey(-3px);
+	}
+
+	/*? write a functions to when we click on the flash card we get the answer*/
 </style>
--- src/routes/flashCards/heather_tests.server.js
+++ src/routes/flashCards/heather_tests.server.js
@@ -158,5 +158,165 @@ export const heathers = [
 		setup: ['a = 2', 'b = 17'],
 		test: 'a <= b',
 		answer: true
+	},
+	{
+		setup: ['m = 12', 'n = 11', 'a = 7', 'b = 5'],
+		test: 'm > n && a + b >= b - m',
+		answer: true
+	},
+	{
+		setup: ['a = 44', 'n = 11', 'm = 33'],
+		test: 'a >= n + m',
+		answer: true
+	},
+	{
+		setup: ['a = 60', 'b = 47', 'm = 17'],
+		test: 'a < b || a > m && b > m',
+		answer: true
+	},
+	{
+		setup: ['a = 40', 'b = 38', 'm = 29', 'n = 12'],
+		test: 'a > b && b > m && m > n',
+		answer: true
+	},
+	{
+		setup: ['a = 11', 'b = 17', 'm = 15', 'n = 12'],
+		test: 'a > m || b > n',
+		answer: true
+	},
+	{
+		setup: ['a = 7', 'b = 7', 'm = 14', 'n = 2'],
+		test: 'a + b = 14 && a * n = 14 && m / n = a',
+		answer: true
+	},
+	{
+		setup: ['a = 11', 'b = 17', 'm = 15', 'n = 12'],
+		test: 'a >= d || n < m && b < m',
+		answer: false
+	},
+	{
+		setup: ['a = 11', 'b = 17', 'm = 15', 'n = 12'],
+		test: 'a > b !== m > n ',
+		answer: true
+	},
+	{
+		setup: ['a = 23', 'b = 89', 'm = 22', 'n = 78'],
+		test: 'n > m && a > b !== b > n ',
+		answer: true
+	},
+	{
+		setup: ['13 % 2 === b', '50 % 2 === a'],
+		test: 'a !== b',
+		answer: true
+	},
+	{
+		setup: ['a = 13', 'b = 17', 'm = 20', 'n = 2'],
+		test: 'a >= n && m % n = 0 && b >= n',
+		answer: true
+	},
+	{
+		setup: ['a = 50', 'b = 33', 'c = 77'],
+		test: 'a + b !== c',
+		answer: true
+	},
+	{
+		setup: ['a = 5 + 22', 'b = 25 - 2'],
+		test: 'a !== b',
+		answer: true
+	},
+	{
+		setup: ['a + b = 7', 'b = 2'],
+		test: 'a + b === b',
+		answer: false
+	},
+	{
+		setup: ['a = 1 - 5', 'b = 5 - 9'],
+		test: 'a !== b',
+		answer: false
+	},
+	{
+		setup: ['a = 7', 'b = 5 % 2', 'm = 1'],
+		test: 'a > m && b === m && b <= a',
+		answer: true
+	},
+	{
+		setup: ['a = 20 % 2', 'b = 7 + 1'],
+		test: 'a !== b',
+		answer: true
+	},
+	{
+		setup: ['a = 1', 'b = 17 % 2'],
+		test: 'a === b',
+		answer: true
+	},
+	{
+		setup: ['a = 0', 'b = 100 % 2'],
+		test: 'a !== b',
+		answer: false
+	},
+	{
+		setup: ['a = 88', 'b = 89', 'm = 88 % 2', 'n = 89 % 2'],
+		test: 'm <= n && a === b',
+		answer: false
+	},
+	{
+		setup: ['a = 70', 'b = 49', 'm = 7 * 7', 'n = 7 * 2'],
+		test: 'm > n && a < b',
+		answer: false
+	},
+	{
+		setup: ['a = 12', 'b = 1', 'm = 2', 'n = 13'],
+		test: 'a <= n && n % m = 1 && a >= n',
+		answer: false
+	},
+	{
+		setup: ['a = 50', 'b = 33', 'm = 80', 'n = 8'],
+		test: 'a + b === m + n',
+		answer: true
+	},
+	{
+		setup: ['a = 23', 'b = 27 - 4'],
+		test: 'a !== b',
+		answer: false
+	},
+	{
+		setup: ['a + b = 7', 'm = 2', 'n = 5'],
+		test: 'a + b === m + n',
+		answer: true
+	},
+	{
+		setup: ['a = 53', 'b = 57 - 4'],
+		test: 'a !== b',
+		answer: false
+	},
+	{
+		setup: ['a = 10', 'b = 20 % 2', 'm = 20'],
+		test: 'a < m && b !== m && b <= a',
+		answer: true
+	},
+	{
+		setup: ['a = 21 % 5', 'b = 7 - 6'],
+		test: 'a !== b',
+		answer: false
+	},
+	{
+		setup: ['a = 2', 'b = 17 % 3'],
+		test: 'a === b',
+		answer: true
+	},
+	{
+		setup: ['a = 0', 'b = 101 % 2'],
+		test: 'a !== b',
+		answer: true
+	},
+	{
+		setup: ['a = 11', 'b = 33', 'm = 11 % 2', 'n = 21 % 2'],
+		test: 'm <= n && a === b',
+		answer: false
+	},
+	{
+		setup: ['a = 7', 'b = 8', 'm = 3 * 6', 'n = 9 * 2'],
+		test: 'm === n && a < b',
+		answer: true
 	}
 ];
