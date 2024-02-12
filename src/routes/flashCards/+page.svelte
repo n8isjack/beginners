a8c7e533c90d06156e69d5bd3076128e91b3e569
angelinewashburn <ballofcharisma@gmail.com>
2024-02-12 13:28:06 -0700

    Adding cooler problems

--- src/routes/flashCards/+page.svelte
+++ src/routes/flashCards/+page.svelte
@@ -36,8 +36,23 @@
 		<div class="test">{currentCard.test}</div>
 
 		<div class="answer">
-			<button class="btn variant-filled" on:click={() => button}>True</button>
-			<button class="btn variant-filled" on:click={() => button}>False</button>
+			<button
+				class="glow-on-hover"
+				type="button"
+				on:click|stopPropagation={() => (currentCard.correct = currentCard.answer === true)}
+				>True</button
+			>
+			<button
+				class="glow-on-hover"
+				type="button"
+				on:click|stopPropagation={() => (currentCard.correct = currentCard.answer === false)}
+				>False</button
+			>
+			{#if currentCard.correct === true}
+				<div class="correct">Got it!!</div>
+			{:else if currentCard.correct === false}
+				<div class="suck">OOPS!!!!</div>
+			{/if}
 		</div>
 	</div>
 </main>
@@ -73,18 +88,105 @@
 		flex-flow: row wrap;
 		justify-content: center;
 		margin: 1em;
+		font-family: impact;
 	}
 	.value {
 		margin: 0.5em;
+		font-family: courier;
 	}
 	.test {
 		margin: 2em;
 		display: flex;
 		flex-flow: row wrap;
 		justify-content: center;
+		font-family: impact;
 	}
 	.answer {
 		margin: 3.5em;
+		border: solid black;
+		padding: 0.5em;
+	}
+	button {
+		margin: 1em;
+	}
+	.glow-on-hover {
+		width: 220px;
+		height: 50px;
+		border: none;
+		outline: none;
+		color: #fff;
+		background: #111;
+		cursor: pointer;
+		position: relative;
+		z-index: 0;
+		border-radius: 10px;
+	}
+	.glow-on-hover:before {
+		content: '';
+		background: linear-gradient(
+			45deg,
+			#ff0000,
+			#ff7300,
+			#fffb00,
+			#48ff00,
+			#00ffd5,
+			#002bff,
+			#7a00ff,
+			#ff00c8,
+			#ff0000
+		);
+		position: absolute;
+		top: -2px;
+		left: -2px;
+		background-size: 400%;
+		z-index: -1;
+		filter: blur(5px);
+		width: calc(100% + 4px);
+		height: calc(100% + 4px);
+		animation: glowing 20s linear infinite;
+		opacity: 0;
+		transition: opacity 0.3s ease-in-out;
+		border-radius: 10px;
+	}
+
+	.glow-on-hover:active {
+		color: #000;
+	}
+
+	.glow-on-hover:active:after {
+		background: transparent;
+	}
+
+	.glow-on-hover:hover:before {
+		opacity: 1;
+	}
+
+	.glow-on-hover:after {
+		z-index: -1;
+		content: '';
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background: #111;
+		left: 0;
+		top: 0;
+		border-radius: 10px;
+	}
+
+	@keyframes glowing {
+		0% {
+			background-position: 0 0;
+		}
+		50% {
+			background-position: 400% 0;
+		}
+		100% {
+			background-position: 0 0;
+		}
+	}
+	.correct {
+	}
+	.suck {
 	}
 	/* CSS Area */
 </style>
